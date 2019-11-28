e51ee84bf212b8afaced6b90d5eeb8c5cb7f74d4
asteinh <armin.steinhauser@kuleuven.be>
2019-11-28 16:28:15 +0100

    build MEX in Octave; lacking support for enumerations

--- tensorflow/+util/+bob/BuildEnvironment.m
+++ tensorflow/+util/+bob/BuildEnvironment.m
@@ -7,7 +7,7 @@ classdef BuildEnvironment < util.mixin.Base
     pkg = [];
     dirs = struct();
     filename = 'tensorflow_m_'; % the name of the resulting MEX interface
-    mexfile = [];
+    executable = [];
   end
 
   methods
@@ -30,26 +30,28 @@ classdef BuildEnvironment < util.mixin.Base
     function prepare(obj)
       obj.dirs.mex = fullfile(obj.pkg, 'mex');
       obj.dirs.out = fullfile(obj.dirs.mex, 'build');
-      obj.mexfile = fullfile(obj.dirs.out, [obj.filename '.' mexext]);
+      obj.executable = fullfile(obj.dirs.out, obj.filename);
 
       % clear possibly loaded MEXs
-      [~, loaded] = inmem();
-      for f = 1:1:numel(loaded)
-        if ~isempty(strfind(loaded{f}, obj.filename))
-          clear(loaded{f});
+      if obj.isMatlab
+        [~, loaded] = inmem();
+        for f = 1:1:numel(loaded)
+          if ~isempty(strfind(loaded{f}, obj.filename))
+            clear(loaded{f});
+          end
         end
       end
 
       % delete existing file with identical name
-      if exist(obj.mexfile, 'file') == 3
-        delete(obj.mexfile);
+      if exist([obj.executable '.' mexext], 'file') == 3
+        delete([obj.executable '.' mexext]);
       end
 
       % cleanup build directory
       files = dir(obj.dirs.out);
       for f = files'
         if ~f.isdir && ~strcmpi(f.name, '.gitkeep')
-          delete(fullfile(f.folder, f.name));
+          delete(fullfile(obj.dirs.out, f.name));
         end
       end
     end
@@ -73,29 +75,49 @@ classdef BuildEnvironment < util.mixin.Base
       % library dependencies
       libs = { '-ltensorflow' };
 
-      % helping Matlab to 'lib_tf.*' when executing the MEX function
-      LD_RUN_PATH = getenv('LD_RUN_PATH');
-      if ~contains(LD_RUN_PATH, fullfile(obj.lib.path, 'lib'))
-        setenv('LD_RUN_PATH', [fullfile(obj.lib.path, '/lib:'), LD_RUN_PATH]);
-      end
-
-      % collected arguments for MEX
-      mexargs = [ {'LDOPTIMFLAGS=-O3'}, ...
-                  {['LDFLAGS=$LDFLAGS,-rpath,' fullfile(obj.lib.path, 'lib')]}, ...
-                  includedirs(:)', ...
-                  { file }, ...
-                  sources(:)', ...
-                  libdirs(:)', ...
-                  libs(:)', ...
-                  {'-outdir'}, { obj.dirs.out }, ...
-                  {'-largeArrayDims'} ...
-                ];
+      % % helping Matlab to 'lib_tf.*' when executing the MEX function
+      % LD_RUN_PATH = getenv('LD_RUN_PATH');
+      % if ~contains(LD_RUN_PATH, fullfile(obj.lib.path, 'lib'))
+      %   setenv('LD_RUN_PATH', [fullfile(obj.lib.path, '/lib:'), LD_RUN_PATH]);
+      % end
 
       % MEXing, possibly in debug mode (= verbose build + debug symbols)
-      if obj.isdebug()
-        mex('-v', '-g',  'CFLAGS=$CFLAGS -std=c99 -DDEBUG -g -ggdb3', mexargs{:});
+      if obj.isMatlab
+        % collected arguments for MEX
+        mexargs = [ {'LDOPTIMFLAGS=-O3'}, ...
+                    {['LDFLAGS=$LDFLAGS,-rpath,' fullfile(obj.lib.path, 'lib')]}, ...
+                    includedirs(:)', ...
+                    { file }, ...
+                    sources(:)', ...
+                    libdirs(:)', ...
+                    libs(:)', ...
+                    {'-outdir'}, { obj.dirs.out }, ...
+                    {'-largeArrayDims'} ...
+                  ];
+
+        if obj.isdebug()
+          mex('-v', '-g',  'CFLAGS=$CFLAGS -std=c99 -DDEBUG -g -ggdb3', mexargs{:});
+        else
+          mex('CFLAGS=$CFLAGS -std=c99', mexargs{:});
+        end
       else
-        mex('CFLAGS=$CFLAGS -std=c99', mexargs{:});
+        % on Octave
+        mkoctargs = [ { ['-Wl,-rpath=' fullfile(obj.lib.path, 'lib')] }, ...
+                      includedirs(:)', ...
+                      { file }, ...
+                      sources(:)', ...
+                      libdirs(:)', ...
+                      libs(:)', ...
+                      { '-o' }, { obj.executable }, ...
+                      { '--mex' } ...
+                    ];
+
+        if obj.isdebug()
+          [a, status] = mkoctfile('-v', '-g', mkoctargs{:});
+          assert(status == 0, 'Irrecoverable error while building interface. Aborting.');
+        else
+          mkoctfile(mkoctargs{:});
+        end
       end
 
       % TODO - find a nicer workaround for this:
