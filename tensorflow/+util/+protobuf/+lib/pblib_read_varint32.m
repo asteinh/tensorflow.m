b76d9f19771fbd8f5c4bd2bd4552c4dacaac9579
asteinh <armin.steinhauser@kuleuven.be>
2019-11-29 15:40:25 +0100

    more fixes for Octave, add method signatures to Ops classdef

--- tensorflow/+util/+protobuf/+lib/pblib_read_varint32.m
+++ tensorflow/+util/+protobuf/+lib/pblib_read_varint32.m
@@ -1,25 +1,25 @@
-function [num, num_read] = pb_read_varint32(buffer, offset)
+function [num, num_read] = pblib_read_varint32(buffer, offset)
 %pb_read_varint32
 %   function [num, num_read] = pb_read_varint32(buffer, offset)
-  
+
 %   protobuf-matlab - FarSounder's Protocol Buffer support for Matlab
 %   Copyright (c) 2008, FarSounder Inc.  All rights reserved.
 %   http://code.google.com/p/protobuf-matlab/
-%  
+%
 %   Redistribution and use in source and binary forms, with or without
 %   modification, are permitted provided that the following conditions are met:
-%  
+%
 %       * Redistributions of source code must retain the above copyright
 %   notice, this list of conditions and the following disclaimer.
-%  
+%
 %       * Redistributions in binary form must reproduce the above copyright
 %   notice, this list of conditions and the following disclaimer in the
 %   documentation and/or other materials provided with the distribution.
-%  
+%
 %       * Neither the name of the FarSounder Inc. nor the names of its
 %   contributors may be used to endorse or promote products derived from this
 %   software without specific prior written permission.
-%  
+%
 %   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 %   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 %   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -44,4 +44,3 @@ function [num, num_read] = pb_read_varint32(buffer, offset)
     index = index + 1;
   end
   num_read = index;
-
