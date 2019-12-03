19581a5e21e67ed0ee9f99c9d420f5c1f3d8bd4b
asteinh <asteinh@users.noreply.github.com>
2019-12-03 11:50:49 +0100

    Support coverage (#4)
    * setup codecov and make preparation in CI separate job
    
    * add codecov shield

--- tests/run_unittests.m
+++ tests/run_unittests.m
@@ -1,13 +1,23 @@
-clear; clc
+function run_unittests(varargin)
+  WITH_COVERAGE = false;
 
-addpath(fullfile(pwd, 'MOxUnit', 'MOxUnit'));
-addpath(fullfile(pwd, 'MOcov', 'MOcov'));
-moxunit_set_path();
+  if nargin > 0 && any(strcmpi('-with_coverage', varargin))
+    WITH_COVERAGE = true;
+  end
 
-pkg_dir = fullfile(pwd, '..', 'tensorflow');
-res_dir = fullfile(pwd, 'results');
+  addpath(fullfile(pwd, 'MOxUnit', 'MOxUnit'));
+  addpath(fullfile(pwd, 'MOcov', 'MOcov'));
+  moxunit_set_path();
 
-moxunit_runtests('unittests', '-verbose', ...
-                 '-with_coverage', '-cover', pkg_dir, ...
-                 '-cover_exclude', '@Ops', '-cover_exclude', '\+util', ...
-                 '-cover_json_file', fullfile(res_dir, 'coverage.json'));
+  pkg_dir = fullfile(pwd, '..', 'tensorflow');
+  if WITH_COVERAGE
+    res_dir = fullfile(pwd, 'results');
+    moxunit_runtests('unittests', '-verbose', ...
+                     '-with_coverage', '-cover', pkg_dir, ...
+                     '-cover_exclude', '@Ops', '-cover_exclude', '\+util', ...
+                     '-cover_html_dir', fullfile(res_dir, 'html'), ...
+                     '-cover_xml_file', fullfile(res_dir, 'coverage.xml'));
+  else
+    moxunit_runtests('unittests', '-verbose');
+  end
+end
--- tests/setup_unittests.sh
+++ tests/setup_unittests.sh
@@ -4,8 +4,8 @@
 rm -rf MOxUnit && rm -rf MOcov && rm -rf results
 
 # clone required repositories
-git clone git@github.com:MOxUnit/MOxUnit.git
-git clone git@github.com:MOcov/MOcov.git
+git clone https://github.com/MOxUnit/MOxUnit.git
+git clone https://github.com/MOcov/MOcov.git
 
 # output directory
 mkdir results
